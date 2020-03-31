6455246d1689c64cbdd471da102d42417e417945
Jonathan <jonathanpalma89@gmail.com>
2020-03-31 20:33:24 +0100

    chore: add Hero component and import to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Hero from '../components/Hero';
 
 function HomePage(props){
   return(
-    <p>HomePage works!</p>
+    <Hero title={props.title} subTitle={props.subTitle} bodyText={props.bodyText}/>
   );
 }
 
